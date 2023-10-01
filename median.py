9b9e1c87e55d30db0dca74302b631b244055dfe6
k22007695 <k22007695@kcl.ac.uk>
2023-10-01 20:11:42 +0100

    Completed cleaned version

--- median.py
+++ median.py
@@ -11,12 +11,11 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        print(numbers)
-        numbers = orderNumbers(numbers)
-        print(numbers)
-        print(findMedian(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
+
+numbers = orderNumbers(numbers)
+print(findMedian(numbers))
 
