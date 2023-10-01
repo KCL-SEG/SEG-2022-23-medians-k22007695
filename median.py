ede586a8656aa85e8f1f674ae308ff11a613dfc8
k22007695 <k22007695@kcl.ac.uk>
2023-10-01 20:08:22 +0100

    Version working soted and median

--- median.py
+++ median.py
@@ -1,12 +1,22 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+import statistics
+#Median Calculator
+
+def orderNumbers(numbers):
+    return (sorted(numbers))
+
+def findMedian(numbers):
+    return (statistics.median(map(float,numbers)))
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(numbers)
+        numbers = orderNumbers(numbers)
+        print(numbers)
+        print(findMedian(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
